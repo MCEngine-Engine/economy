3b484fa73bc1d200ded8fe5cf3c1d7889bd082f9
JetsadaWijit <jetsadawijit@outlook.com>
2025-06-03 07:14:37 +0000

    chore: Created complete codebase

---
+++ server/papermc/engine/src/main/java/io/github/mcengine/papermc/currency/engine/MCEngineCurrencyPaperMC.java
@@ -0,0 +1,36 @@
+package io.github.mcengine.papermc.currency.engine;
+
+import io.github.mcengine.api.mcengine.MCEngineApi;
+import io.github.mcengine.api.mcengine.Metrics;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MCEngineCurrencyPaperMC extends JavaPlugin {
+    /**
+     * Called when the plugin is enabled.
+     * Performs configuration loading, token validation, API initialization, and schedules token validation checks.
+     */
+    @Override
+    public void onEnable() {
+        new Metrics(this, 26077);
+        saveDefaultConfig(); // Save config.yml if it doesn't exist
+
+        boolean enabled = getConfig().getBoolean("enable", false);
+        if (!enabled) {
+            getLogger().warning("Plugin is disabled in config.yml (enable: false). Disabling plugin...");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        // Load extensions
+        MCEngineApi.loadExtensions(this, "addons", "AddOn");
+        MCEngineApi.loadExtensions(this, "dlcs", "DLC");
+
+        MCEngineApi.checkUpdate(this, "github", "MCEngine", "currency-engine", getConfig().getString("github.token", "null"));
+    }
+
+    /**
+     * Called when the plugin is disabled.
+     */
+    @Override
+    public void onDisable() {}
+}
